714e989a7bce0bcd2727f2625f2195fd5580f47c
Sonakshi Manchanda <isonakshi@gmail.com>
2012-11-26 18:09:41 +0530

    Update examples/jive-core/search-content/search-content.js

--- examples/jive-core/search-content/search-content.js
+++ examples/jive-core/search-content/search-content.js
@@ -6,7 +6,7 @@ function init() {
 // Perform a search and display the results
 function search() {
  
-     osapi.jive.corev3.contents.get({"author":"@self","fields": "@all"}).execute(function(response) {
+     osapi.jive.corev3.contents.get({"author.id":"2065","fields": "@all"}).execute(function(response) {
         console.log("The Response is " + JSON.stringify(response));
         
            
